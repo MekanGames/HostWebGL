3006ec8503660ab0cdfd065dbee87397eeb73161
Bradley Mauncho <bradley@mekangames.com>
2024-11-12 11:06:17 +0300

    Remove site lock

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "MekanGames-Human Tower-0.1.0";
 const contentToCache = [
-    "Build/fb7a1d64a0e05fc66279514d058b3a56.loader.js",
-    "Build/d72f3918dcb3eca7150344f9f35ae9bf.framework.js",
-    "Build/9ce49b39243c0b0910137c770b04c807.data",
-    "Build/0e6a8303d61ed0ca6873a635f3a78c0a.wasm",
+    "Build/ceccf83993b3e1498228ba598c85582c.loader.js",
+    "Build/d72f3918dcb3eca7150344f9f35ae9bf.framework.js.br",
+    "Build/509f9034dc277ec7ee4686e9d8952787.data.br",
+    "Build/03eeeccc3ac57a8ef729d7a2a2618aec.wasm.br",
     "TemplateData/style.css"
 
 ];
