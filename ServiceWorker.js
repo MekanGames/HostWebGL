558f3150a3ad88429d6b122e765060698c4cc69f
Bradley Mauncho <bradley@mekangames.com>
2024-11-14 12:18:31 +0300

    Evans - Updates

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "MekanGames-Human Tower-0.1.0";
 const contentToCache = [
-    "Build/9cccb97b3de32c6cbec0c19480228100.loader.js",
+    "Build/3a791f451af3dfd56e9ee0defe085342.loader.js",
     "Build/8d21b1a93c0ea05516b905f419ca9eee.framework.js",
-    "Build/fcd99e0ed0890f760ceb4c462e511c6c.data",
-    "Build/22afb0c2646f0cb6e4ce8d19d6907c95.wasm",
+    "Build/754908822b5f7b3ec96917fae3e7ab9f.data",
+    "Build/d0fbeb1d98d5cea743a99ff92b147c2d.wasm",
     "TemplateData/style.css"
 
 ];
